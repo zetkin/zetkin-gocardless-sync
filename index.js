fd2cd5d61f6ffa253727bb56a4c790140a00e44a
Richard Olsson <r@richardolsson.se>
2021-06-14 09:54:13 +0200

    Add Zetkin initialization

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const constants = require('gocardless-nodejs/constants');
 const gocardless = require('gocardless-nodejs');
 
 const config = require('./config.json');
+const { initZetkin } = require('./lib/zetkin');
 
 const client = gocardless(config.GOCARDLESS_KEY, constants.Environments.live);
 
@@ -30,10 +31,21 @@ const loadAll = async (resource, filters) => {
 };
 
 (async () => {
+	const Z = await initZetkin(config);
+	try {
+		const data = await Z.resource('users', 'me').get();
+		console.log(data);
+	}
+	catch (err) {
+		console.error(err);
+	}
+
+	/*
 	const payments = await loadAll(client.payments, {
 	    created_at: {
 	        gt: '2021-01-01T00:00:00.000Z',
 	        lt: '2021-02-01T00:00:00.000Z',
 	    },
 	});
+	*/
 })();
