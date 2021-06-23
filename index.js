a7a4d25dd0239aff384c77a26e2d4f4af7fcc09e
Richard Olsson <r@richardolsson.se>
2021-06-23 10:31:11 +0200

    Format all output as YAML

--- index.js
+++ index.js
@@ -7,67 +7,24 @@ const syncUtil = require('./lib/syncUtil');
     gocardless.initClient(config);
 
     const z = await initZetkin(config);
-    /*
-    try {
-        const data = await Z.resource('session').get();
-        console.log(data);
-    }
-    catch (err) {
-        console.error(err);
-    }
-
-    const payments = await gocardless.loadPayments({
-        created_at: {
-            gt: '2021-01-01T00:00:00.000Z',
-            lt: '2021-02-01T00:00:00.000Z',
-        },
-    });
-    console.log(JSON.stringify(payments, null, '  '));
-
-    const data = await gocardless.loadMandates();
-    console.log(JSON.stringify(data, null, '  '));
-    */
-
-    /*
-    try {
-        const people = await Z.resource('orgs', config.ZETKIN_ORG_ID, 'people').get();
-        const data = people.data.data;
-        console.log(JSON.stringify(data, null, '  '));
-    }
-    catch (err) {
-        console.error(err);
-    }
-    */
-
     const syncer = await syncUtil.init(gocardless, z, config);
+    const reports = [];
 
-    report = await syncer.syncMonth(2021, 1);
-    console.log('Found: ', report.foundInZetkin.length);
-    console.log('Missing: ', report.missingInZetkin.length);
-    console.log('Unpaid: ', Object.values(report.zetkinChecklist).filter(found => !found).length);
-    console.log('Errors', report.errors);
+    reports.push(await syncer.syncMonth(2021, 0));
+    reports.push(await syncer.syncMonth(2021, 1));
+    reports.push(await syncer.syncMonth(2021, 2));
+    reports.push(await syncer.syncMonth(2021, 3));
+    reports.push(await syncer.syncMonth(2021, 4));
+    reports.push(await syncer.syncMonth(2021, 5));
 
-    report = await syncer.syncMonth(2021, 2);
-    console.log('Found: ', report.foundInZetkin.length);
-    console.log('Missing: ', report.missingInZetkin.length);
-    console.log('Unpaid: ', Object.values(report.zetkinChecklist).filter(found => !found).length);
-    console.log('Errors', report.errors);
-
-    report = await syncer.syncMonth(2021, 3);
-    console.log('Found: ', report.foundInZetkin.length);
-    console.log('Missing: ', report.missingInZetkin.length);
-    console.log('Unpaid: ', Object.values(report.zetkinChecklist).filter(found => !found).length);
-    console.log('Errors', report.errors);
-
-    report = await syncer.syncMonth(2021, 4);
-    console.log('Found: ', report.foundInZetkin.length);
-    console.log('Missing: ', report.missingInZetkin.length);
-    console.log('Unpaid: ', Object.values(report.zetkinChecklist).filter(found => !found).length);
-    console.log('Errors', report.errors);
-
-    report = await syncer.syncMonth(2021, 5);
-    console.log('Found: ', report.foundInZetkin.length);
-    console.log('Missing: ', report.missingInZetkin.length);
-    console.log('Unpaid: ', Object.values(report.zetkinChecklist).filter(found => !found).length);
-    console.log('Errors', report.errors);
+    console.log('reports:');
+    reports.forEach(report => {
+        console.log('  - date: ', report.date);
+        console.log('    duration: ', report.duration);
+        console.log('    found: ', report.foundInZetkin.length);
+        console.log('    missing: ', report.missingInZetkin.length);
+        console.log('    ignored: ', report.ignored.length);
+        console.log('    unpaid: ', Object.values(report.zetkinChecklist).filter(found => !found).length);
+        console.log('    errors: ', report.errors.length);
+    });
 })();
